eab7e4a5c11f9980a4fcafacf4ad44fbe89fbd97
Jean-Paul Ladage <j.ladage@zestsoftware.nl>
2018-10-02 11:21:49 +0200

    Late boolean

--- awesome_tshirt/static/src/js/image_preview.js
+++ awesome_tshirt/static/src/js/image_preview.js
@@ -2,6 +2,7 @@ odoo.define('awesome_tshirt.PreviewImage', function (require) {
     "use strict";
 
     var FieldChar = require('web.basic_fields').FieldChar;
+    var FieldBoolean = require('web.basic_fields').FieldBoolean;
     var registry = require('web.field_registry');
 
     var PreviewImage = FieldChar.extend({
@@ -14,13 +15,31 @@ odoo.define('awesome_tshirt.PreviewImage', function (require) {
                    src: this.value,
                    width: 300
                  }));
-            }
-            else {
+            } else {
                 this.$el.html($('<p>IMAGE MISSING</p>'));
 
             }
         },
     });
 
+    var LateBoolean = FieldBoolean.extend({
+        _renderReadonly: function () {
+            this._super();
+            if (this.value) {
+                if (this.value == true){
+                    //render green circle
+                    console.log('Is late is true')
+
+                } else {
+                    console.log('Is late is false')
+                    //render red cirle
+
+                }
+            }
+        },
+    });
+
     registry.add('image-preview-field', PreviewImage);
+    registry.add('late-boolean', LateBoolean);
+
 });
