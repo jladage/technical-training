b2896f27cf49caf6065d571b898db032a52e6265
Jean-Paul Ladage <j.ladage@zestsoftware.nl>
2018-10-02 10:48:06 +0200

    Another try

--- awesome_tshirt/static/src/js/image_preview.js
+++ awesome_tshirt/static/src/js/image_preview.js
@@ -1,24 +1,26 @@
-odoo.define('awesome_tshirt.ChartWidget', function (require) {
+odoo.define('awesome_tshirt.PreviewImage', function (require) {
     "use strict";
 
     var FieldChar = require('web.basic_fields').FieldChar;
+    var registry = require('web.field_registry');
 
     var PreviewImage = FieldChar.extend({
         _renderReadonly: function () {
             // implement some custom logic here
-            console.log('Yeah!!!!   ')
-            var self = this;
-            var img = $('<img />',
-             { id: 'image-preview',
-               src: this.value,
-               width: 300
-             })
-              .appendTo(this.el);
+            console.log('Yeah!!!!   ');
+            if this.isSet() {
+                this.
+            }
+            if (this.value) {
+                var img = $('<img />',
+                 { id: 'image-preview',
+                   src: this.value,
+                   width: 300
+                 })
+                  .appendTo(this.$el);
+            }
         },
     });
 
-    var fieldRegistry = require('web.field_registry');
-
-    fieldRegistry.add('image-preview-field', PreviewImage);
-
+    registry.add('image-preview-field', PreviewImage);
 });
