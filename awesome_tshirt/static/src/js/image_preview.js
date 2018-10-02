18c6fc96d38b6755c5009cad0dcff51dfd5b7f8c
Jean-Paul Ladage <j.ladage@zestsoftware.nl>
2018-10-02 12:03:32 +0200

    Cleanup old code

--- awesome_tshirt/static/src/js/image_preview.js
+++ awesome_tshirt/static/src/js/image_preview.js
@@ -2,14 +2,11 @@ odoo.define('awesome_tshirt.PreviewImage', function (require) {
     "use strict";
 
     var FieldChar = require('web.basic_fields').FieldChar;
-    var FieldBoolean = require('web.basic_fields').FieldBoolean;
     var registry = require('web.field_registry');
-    var Widget = require('web.Widget');
 
     var PreviewImage = FieldChar.extend({
         _renderReadonly: function () {
             // implement some custom logic here
-            console.log('Yeah!!!!');
             if (this.value) {
                 this.$el.html($('<img />',
                  { id: 'image-preview',
@@ -17,7 +14,7 @@ odoo.define('awesome_tshirt.PreviewImage', function (require) {
                    width: 300
                  }));
             } else {
-                this.$el.text(_("MISSING TSHIRT DESIGN"));
+                this.$el.text(_("MISSING T-SHIRT DESIGN"));
                 this.$el.addClass('alert-danger');
             }
         },
@@ -30,36 +27,7 @@ odoo.define('awesome_tshirt.PreviewImage', function (require) {
         },
     });
 
-    var LateBoolean = FieldBoolean.extend({
-        _renderReadonly: function () {
-            this._super();
-            if (this.value) {
-                if (this.value == true){
-                    //render green circle
-                    console.log('Is late is true')
-
-                } else {
-                    console.log('Is late is false')
-                    //render red cirle
-
-                }
-            }
-        },
-    });
-
-    var IsLateMessage = Widget.extend({
-        init: function (parent, dataPoint) {
-            this.data = dataPoint.data;
-        },
-        start: function () {
-            this.$el.text(this.data.foo + "!");
-        },
-        updateState: function (dataPoint) {
-            this.$el.text(dataPoint.data.foo + "!");
-        },
-    });
-
     registry.add('image-preview-field', PreviewImage);
-    registry.add('late-boolean', LateBoolean);
+    return PreviewImage;
 
 });
