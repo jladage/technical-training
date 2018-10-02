83d09af80e7822b64c97c8be3c222568eaafe803
Jean-Paul Ladage <j.ladage@zestsoftware.nl>
2018-10-02 13:19:01 +0200

    Another try

--- awesome_tshirt/static/src/js/message_widget.js
+++ awesome_tshirt/static/src/js/message_widget.js
@@ -1,27 +1,38 @@
 odoo.define('awesome_tshirt.message_widget', function (require) {
     "use strict";
 
+    var core = require('web.core');
     var registry = require('web.widget_registry');
     var Widget = require('web.Widget');
-
+    var qweb = core.qweb;
     var MessageWidget = Widget.extend({
-        init: function (parent, dataPoint) {
+        init: function (parent, record) {
             //access is_late field and set message text
-            this.data = dataPoint.data;
+            this._super.apply(this, arguments);
+            this.record = record;
         },
+
         start: function () {
-            this.$el.html($('<div>',{
-                'class':'alert alert-info',
-                'value':'My message',
-            }));
+            this._render();
+            return this._super.apply(this, arguments);
         },
-        updateState: function (dataPoint) {
-            this.$el.text(dataPoint.data.foo + "!");
+
+        updateState: function (record) {
+            this.record = record;
+            this._render();
         },
-        destroy: function (dataPoint) {
+        
+        _render: function () {
             this.$el.empty();
+            if (!this.record.data.image_url) {
+                this.$el.append(qweb.render('WarningWidget.NoImage'));
+            }
+            if (this.record.data.amount > 100) {
+                this.$el.append(qweb.render('WarningWidget.AddPromo'));
+            }
         }
     });
 
     registry.add('message-widget', MessageWidget);
+    return MessageWidget;
 });
